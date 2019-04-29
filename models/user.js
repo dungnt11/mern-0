f23c0c71aa637ef7d9da9ffc0ae40b50843808d6
nihilismJS <nihilism.core@gmail.com>
2019-04-29 13:47:58 -0700

    done create new user

---
+++ models/user.js
@@ -0,0 +1,26 @@
+const mongoose = require("mongoose");
+const schema = mongoose.Schema;
+
+const userSchema = schema({
+  name: {
+    type: String,
+    require: true
+  },
+  pwd: {
+    type: String,
+    require: true
+  },
+  email: {
+    type: String,
+    require: true
+  },
+  avatar: {
+    type: String
+  },
+  date: {
+    type: String,
+    default: Date.now()
+  }
+});
+
+module.exports = mongoose.model("users", userSchema);
--- routes/apis/user.js
+++ routes/apis/user.js
@@ -1,10 +1,46 @@
-const express = require('express');
-const routerUser = express.Router();
 /**
  * @api/user
  */
-routerUser.get('/', (req, res) => {
-  res.json({msg: 'get user api'})
-})
+const express = require("express");
+const routerUser = express.Router();
+const user = require("../../models/user");
+const gravatar = require("gravatar");
+const bcrypt = require("bcryptjs");
+routerUser.post("/register", (req, res) => {
+  user.findOne({ email: req.body.name }).then(us => {
+    if (us) {
+      res.status(400).json({
+        msg: "Email already exists"
+      });
+    } else {
+      // lay anh tu website bang email cho truoc
+      const avatar = gravatar.url(req.body.email, {
+        s: "200",
+        r: "pg",
+        d: "mm"
+      });
+
+      // create new user
+      const newUser = new user({
+        name: req.body.name,
+        email: req.body.email,
+        pwd: req.body.pwd,
+        avatar
+      });
+      console.log(newUser);
+      //hash pwd
+      bcrypt.genSalt(10, function(err, salt) {
+        bcrypt.hash(newUser.pwd, salt, function(err, hash) {
+          if (err) throw err;
+          newUser.pwd = hash;
+          newUser
+            .save()
+            .then(user => res.status(200).json(user))
+            .catch(err => console.log(err));
+        });
+      });
+    }
+  });
+});
 
 module.exports = routerUser;
