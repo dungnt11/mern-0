856b83d40a6556987746525f9ef37f135d81b5f5
nihilismJS <nihilism.core@gmail.com>
2019-05-01 23:45:35 -0700

    done test prolife

--- routes/apis/prolife.js
+++ routes/apis/prolife.js
@@ -1,10 +1,34 @@
-const express = require('express');
+const express = require("express");
 const routerUser = express.Router();
+const mongoose = require("mongoose");
+const passport = require("passport");
+
+// Model Schema
+const ProlifeSchema = require("../../models/Prolife");
+const User = require("../../models/user");
+
 /**
- * @api/prolife
+ * @route   api/prolife
+ * @desc    Prolife user
+ * @access  Public
  */
-routerUser.get('/', (req, res) => {
-  res.json({msg: 'get user api'})
-})
+
+routerUser.get(
+  "/",
+  passport.authenticate("jwt", { session: false }),
+  (req, res) => {
+    let err = {};
+    // tim kiem profile theo id duoc cung cap boi jwt
+    ProlifeSchema.findOne({ user: req.user.id })
+      .then(prolife => {
+        if (!prolife) {
+          err.noprofile = "There is no prolife for this user";
+          return res.status(404).json(err);
+        }
+        res.status(200).json(prolife);
+      })
+      .catch(err => res.status(404).json(err));
+  }
+);
 
 module.exports = routerUser;
