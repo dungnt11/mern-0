2d32717c3f4d41aea164ebeafb34c791b6f6588b
nihilismJS <nihilism.core@gmail.com>
2019-05-01 22:19:29 -0700

    fixed lan 1 validator in register and login

--- validators/login.js
+++ validators/login.js
@@ -2,21 +2,33 @@ const validator = require("validator");
 const isEmpty = require("./isEmpty");
 
 module.exports = validatorLogin = data => {
-  let err = [];
-
+  let err = {
+    email: [],
+    pwd: []
+  };
   // check undefined
-  if (data.email === void 0 || data.pwd === void 0) {
-    err.push("Username or Password is undefined !");
-  }
-  // check name, email, pwd not null
-  data.email = !isEmpty(data.email) ? data.email : "";
-  data.pwd = !isEmpty(data.pwd) ? data.pwd : "";
+  if (
+    (data.email === void 0 && err.email.push("Email is undefined")) ||
+    (data.pwd === void 0 && err.pwd.push("Password is undefined"))
+  );
+  else {
+    // check name, email, pwd not null
+    data.email = !isEmpty(data.email) ? data.email : "";
+    data.pwd = !isEmpty(data.pwd) ? data.pwd : "";
 
-  if (validator.isEmpty(data.email)) {
-    err.push("Email feild is required !");
+    if (validator.isEmpty(data.email)) {
+      err.email.push("Email feild is required !");
+    }
+    if (validator.isEmpty(data.pwd)) {
+      err.pwd.push("Password feild is required !");
+    }
   }
-  if (validator.isEmpty(data.pwd)) {
-    err.push("Password feild is required !");
+
+  // delete element empty in object err
+  for (let keyOfErr of Object.keys(err)) {
+    if (!err[keyOfErr].length) {
+      delete err[keyOfErr];
+    }
   }
 
   return {
