c6b1fe18eadb63b09d75c6be4c2ecca78324357b
nihilismJS <nihilism.core@gmail.com>
2019-05-10 01:03:20 +0700

    done state login & register

--- client/src/components/auth/Login.js
+++ client/src/components/auth/Login.js
@@ -1,7 +1,64 @@
 import React, { Component } from "react";
 
 export default class Login extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      email: "",
+      pwd: ""
+    };
+  }
+
+  watchSubmit = event => {
+    event.preventDefault();
+    const userLogin = {
+      email: this.state.email,
+      pwd: this.state.pwd
+    };
+    console.log(userLogin);
+  };
+
+  setValue = event => {
+    this.setState({ [event.target.name]: event.target.value });
+  };
+
   render() {
-    return <div>Login</div>;
+    return (
+      <div className="login">
+        <div className="container">
+          <div className="row">
+            <div className="col-md-8 m-auto">
+              <h1 className="display-4 text-center">Log In</h1>
+              <p className="lead text-center">
+                Sign in to your DevConnector account
+              </p>
+              <form onSubmit={this.watchSubmit}>
+                <div className="form-group">
+                  <input
+                    type="email"
+                    className="form-control form-control-lg"
+                    placeholder="Email Address"
+                    name="email"
+                    value={this.state.email}
+                    onChange={this.setValue}
+                  />
+                </div>
+                <div className="form-group">
+                  <input
+                    type="password"
+                    className="form-control form-control-lg"
+                    placeholder="Password"
+                    name="pwd"
+                    value={this.state.pwd}
+                    onChange={this.setValue}
+                  />
+                </div>
+                <input type="submit" className="btn btn-info btn-block mt-4" />
+              </form>
+            </div>
+          </div>
+        </div>
+      </div>
+    );
   }
 }
--- client/src/components/auth/Register.js
+++ client/src/components/auth/Register.js
@@ -1,7 +1,100 @@
-import React, { Component } from "react";
+import React, { Component, Fragment } from "react";
 
 export default class Register extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      name: "",
+      email: "",
+      pwd: "",
+      pwd1: "",
+      err: {}
+    };
+  }
+  watchChange = e => {
+    this.setState({
+      [e.target.name]: e.target.value
+    });
+  };
+
+  sendSubmit = event => {
+    event.preventDefault();
+    const newUser = {
+      name: this.state.name,
+      email: this.state.email,
+      pwd: this.state.pwd,
+      pwd1: this.state.pwd1
+    };
+    console.log(newUser);
+  };
   render() {
-    return <div>Register</div>;
+    return (
+      <Fragment>
+        <div className="register">
+          <div className="container">
+            <div className="row">
+              <div className="col-md-8 m-auto">
+                <h1 className="display-4 text-center">Sign Up</h1>
+                <p className="lead text-center">
+                  Create your DevConnector account
+                </p>
+                <form onSubmit={this.sendSubmit}>
+                  <div className="form-group">
+                    <input
+                      type="text"
+                      className="form-control form-control-lg"
+                      placeholder="Name"
+                      name="name"
+                      required
+                      value={this.state.name}
+                      onChange={this.watchChange}
+                    />
+                  </div>
+                  <div className="form-group">
+                    <input
+                      type="email"
+                      className="form-control form-control-lg"
+                      placeholder="Email Address"
+                      name="email"
+                      value={this.state.email}
+                      onChange={this.watchChange}
+                    />
+                    <small className="form-text text-muted">
+                      This site uses Gravatar so if you want a profile image,
+                      use a Gravatar email
+                    </small>
+                  </div>
+                  <div className="form-group">
+                    <input
+                      type="password"
+                      className="form-control form-control-lg"
+                      placeholder="Password"
+                      name="pwd"
+                      value={this.state.pwd}
+                      onChange={this.watchChange}
+                    />
+                  </div>
+                  <div className="form-group">
+                    <input
+                      type="password"
+                      className="form-control form-control-lg"
+                      placeholder="Confirm Password"
+                      name="pwd1"
+                      value={this.state.pwd1}
+                      onChange={this.watchChange}
+                    />
+                  </div>
+                  <input
+                    type="submit"
+                    className="btn btn-info btn-block mt-4"
+                  />
+                </form>
+              </div>
+            </div>
+          </div>
+        </div>
+      </Fragment>
+    );
   }
 }
